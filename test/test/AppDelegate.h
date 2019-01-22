5fed3e75fe81dbd15bc6bad277f6383b82b607b9
wuhy <wuhy@feifanuniv.com>
2019-01-22 15:55:57 +0800

    fix

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by why on 2019/1/22.
+//  Copyright © 2019 why. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by why on 2019/1/22.
+//  Copyright © 2019 why. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
