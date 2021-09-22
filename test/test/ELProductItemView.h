2be99bc9a4c19c7ee1167d391979f7e29a4e89b3
wuhy <38172519+wuhuiy@users.noreply.github.com>
2021-09-22 14:45:47 +0800

    fix

---
+++ test/test/ELProductItemView.h
@@ -0,0 +1,17 @@
+//
+//  ELProductItemView.h
+//  test
+//
+//  Created by 吴小宇 on 2021/9/22.
+//  Copyright © 2021 why. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ELProductItemView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ test/test/ELProductItemView.m
@@ -0,0 +1,23 @@
+//
+//  ELProductItemView.m
+//  test
+//
+//  Created by 吴小宇 on 2021/9/22.
+//  Copyright © 2021 why. All rights reserved.
+//
+
+#import "ELProductItemView.h"
+
+@implementation ELProductItemView
+
+- (void)initUI {
+    /*
+    itemImageView
+    closeBtn
+    titleLabel
+    priceLabel
+    similarBtn
+     */
+}
+
+@end
